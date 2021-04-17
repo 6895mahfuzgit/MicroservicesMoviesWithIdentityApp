97b18ab0a6baf46b7c197ae6b13a0e1ba3e1fcb0
6895mahfuzgit <6895mahfuz@gmail.com>
2021-04-17 23:43:19 +0600

    Created MoviesController and Add Its's CRUD Operations.

--- Movies.API/Controllers/MoviesController.cs
+++ Movies.API/Controllers/MoviesController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Movies.API.Models;
+using Movies.API.Repositories.EntityRepository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,7 +13,88 @@ namespace Movies.API.Controllers
     [ApiController]
     public class MoviesController : ControllerBase
     {
+        private readonly IMovieRepository _movieRepository;
 
+        public MoviesController(IMovieRepository movieRepository)
+        {
+            _movieRepository = movieRepository;
+        }
 
+        [HttpGet]
+        public async Task<ActionResult<IReadOnlyList<Movie>>> GetMovie()
+        {
+            return Ok(await _movieRepository.GetAllAsync());
+        }
+
+        
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Movie>> GetMovie(int id)
+        {
+            var movie = await _movieRepository.GetByIdAsync(id);
+
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return movie;
+        }
+
+        
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutMovie(int id, Movie movie)
+        {
+            if (id != movie.Id)
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+                await _movieRepository.UpdateAsync(movie);
+            }
+            catch (Microsoft.EntityFrameworkCore.DbUpdateConcurrencyException)
+            {
+                if (!MovieExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        
+        [HttpPost]
+        public async Task<ActionResult<Movie>> PostMovie(Movie movie)
+        {
+            await _movieRepository.AddAsync(movie);
+            return CreatedAtAction("GetMovie", new { id = movie.Id }, movie);
+        }
+
+     
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Movie>> DeleteMovie(int id)
+        {
+            var movie = await _movieRepository.GetByIdAsync(id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            await _movieRepository.DeleteAsync(movie);
+
+            return movie;
+        }
+
+        private bool MovieExists(int id)
+        {
+            var movie = _movieRepository.GetByIdAsync(id);
+            return movie != null;
+        }
     }
 }
