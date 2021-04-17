01326944631ef3f6b0526ad4b886bc78f3663027
6895mahfuzgit <6895mahfuz@gmail.com>
2021-04-17 22:27:23 +0600

    Created Initial Migration For Movie Entity And Update The Migration In Database.

---
+++ Movies.API/Context/ApplicationDBContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Movies.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movies.API.Context
+{
+    public class ApplicationDBContext : DbContext
+    {
+
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
