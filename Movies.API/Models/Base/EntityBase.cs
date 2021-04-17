7036fd7b7f2f95d0528c5d5c7481d56ab7404af0
6895mahfuzgit <6895mahfuz@gmail.com>
2021-04-17 23:05:13 +0600

    Added Base Entity And Base Repository.

---
+++ Movies.API/Models/Base/EntityBase.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movies.API.Models.Base
+{
+    public abstract class EntityBase<TId> : IEntityBase<TId>
+    {
+        public virtual TId Id { get; protected set; }
+
+        int? _requestedHasCode;
+
+        public bool IsTransient()
+        {
+            return Id.Equals(default(TId));
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || !(obj is EntityBase<TId>))
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            if (GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            var item = (EntityBase<TId>)obj;
+            if (item.IsTransient() || IsTransient())
+            {
+                return false;
+            }
+            else
+            {
+                return item == this;
+            }
+
+        }
+
+        public override int GetHashCode()
+        {
+            if (!IsTransient())
+            {
+                if (!_requestedHasCode.HasValue)
+                    _requestedHasCode = Id.GetHashCode() ^ 31;
+                return _requestedHasCode.Value;
+
+            }
+            else
+            {
+                return base.GetHashCode();
+            }
+        }
+
+        public static bool operator ==(EntityBase<TId> left, EntityBase<TId> right)
+        {
+            if (Equals(left, null))
+                return Equals(right, null) ? true : false;
+            else
+                return left.Equals(right);
+        }
+
+
+        public static bool operator !=(EntityBase<TId> left, EntityBase<TId> right)
+        {
+            return !(left == right);
+        }
+
+    }
+}
