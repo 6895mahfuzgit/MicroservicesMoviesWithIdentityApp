d909233e91fc1dbdaefa2df6157e586dfd34442c
6895mahfuzgit <6895mahfuz@gmail.com>
2021-04-18 13:38:38 +0600

    Installed Identity Server nuget Package and Register In DI.

--- IdentityServerApp/Startup.cs
+++ IdentityServerApp/Startup.cs
@@ -16,6 +16,8 @@ namespace IdentityServerApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //for identity Server
+            services.AddIdentityServer();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,6 +29,8 @@ namespace IdentityServerApp
             }
 
             app.UseRouting();
+            //for identity Server
+            app.UseIdentityServer();
 
             app.UseEndpoints(endpoints =>
             {
