b8d2c7cf9fea1d2d7817f069a8b652ef3fa8879f
6895mahfuzgit <6895mahfuz@gmail.com>
2021-04-19 22:51:08 +0600

    Added ApiServices And Remove Entity Packages and Auto Generate Code.

---
+++ Movies.Client/ApiServices/MovieApiService.cs
@@ -0,0 +1,55 @@
+﻿using Movies.Client.Models;
+using Movies.Client.SeedMockData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movies.Client.ApiServices
+{
+    public class MovieApiService : IMovieApiService
+    {
+        public async Task<Movie> CreateMovie(Movie movie)
+        {
+            try
+            {
+                return movie;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public Task DeleteMovie(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Movie> GetMovie(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Movie>> GetMovies()
+        {
+            try
+            {
+                var movies = SeedData.GetMockMovies();
+                return await Task.FromResult(movies);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+
+        }
+
+        public Task<Movie> UpdateMovie(Movie movie)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Movies.Client/Models/Movie.cs
+++ Movies.Client/Models/Movie.cs
@@ -1,8 +1,5 @@
-﻿using Movies.Client.Models.Base;
+﻿
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Movies.Client.Models
 {
