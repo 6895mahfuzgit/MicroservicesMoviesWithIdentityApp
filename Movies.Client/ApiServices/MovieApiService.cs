ae47a0197cde22dd124faa9d593f0e6154689382
6895mahfuzgit <6895mahfuz@gmail.com>
2021-04-25 02:29:34 +0600

    Make Movies API Http Call For Get Movie List From Database.

--- Movies.Client/ApiServices/MovieApiService.cs
+++ Movies.Client/ApiServices/MovieApiService.cs
@@ -1,14 +1,26 @@
-﻿using Movies.Client.Models;
+﻿using IdentityModel.Client;
+using Microsoft.Extensions.Configuration;
+using Movies.Client.Models;
 using Movies.Client.SeedMockData;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace Movies.Client.ApiServices
 {
     public class MovieApiService : IMovieApiService
     {
+
+        private readonly IConfiguration _configuration;
+
+        public MovieApiService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         public async Task<Movie> CreateMovie(Movie movie)
         {
             try
@@ -36,8 +48,48 @@ namespace Movies.Client.ApiServices
         {
             try
             {
-                var movies = SeedData.GetMockMovies();
-                return await Task.FromResult(movies);
+
+                var apiClientCredentials = new ClientCredentialsTokenRequest
+                {
+                    Address = $"{_configuration.GetValue<string>("IdentityServer:ServerURL")}/connect/token",
+                    ClientId = "movieClient",
+                    ClientSecret = _configuration.GetValue<string>("IdentityServer:ClientSecret"),
+                    Scope = _configuration.GetValue<string>("IdentityServer:ScopeOption3")
+                };
+
+                var client = new HttpClient();
+
+                //Check Identity Server Status
+                var identityServerEcho = await client.GetDiscoveryDocumentAsync(_configuration.GetValue<string>("IdentityServer:ServerURL"));
+                if (identityServerEcho.IsError)
+                {
+                    throw new Exception("Identity Server Is not running.");
+                }
+
+
+                //Get Token From Identity Server
+                var tokenResponse = await client.RequestClientCredentialsTokenAsync(apiClientCredentials);
+                if (tokenResponse.IsError)
+                {
+                    throw new Exception("Failed to Get token From Identity Server.");
+                }
+
+
+                var apiClient = new HttpClient();
+                apiClient.SetBearerToken(tokenResponse.AccessToken);
+
+                var responseFromApi = await apiClient.GetAsync("https://localhost:5001/api/Movies");
+                responseFromApi.EnsureSuccessStatusCode();
+
+                //Convert Response in String 
+                var content = await responseFromApi.Content.ReadAsStringAsync();
+
+                //De
+                var movieList = JsonConvert.DeserializeObject<List<Movie>>(content);
+
+                return movieList;
+                //var movies = SeedData.GetMockMovies();
+                //return await Task.FromResult(movies);
             }
             catch (Exception ex)
             {
